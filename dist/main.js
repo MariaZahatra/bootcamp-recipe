269d3b28b4ace5906e3f7ae94e2ae08d003835bc
MariaZahatra <93514913+MariaZahatra@users.noreply.github.com>
2021-11-23 13:30:47 +0200

    Add files via upload

---
+++ dist/main.js
@@ -0,0 +1,28 @@
+
+
+const source = $("#ingredient-template").html();
+const template = Handlebars.compile(source);
+
+
+const fetchData = function(){
+    let ingredient = $("ingredient-input").val();
+    
+    $.get(`/recipes/${ingredient}`, function (response) {
+        console.log(response)
+        $("body").append(`<div>${response[0].firstName} - ${response[0].lastName}</div`)
+        renderrecipes(response)
+
+    })
+
+}
+
+const renderingredient = function(ingredient){
+    const data = {ingredient:ingredient}
+    const newHTML = template(data);
+    console.log(newHTML)
+    $(".ingredient").append(newHTML);
+}
+
+const title = document.getElementsByClassName("content-title")
+title.innerHTML= "Recipes"
+
